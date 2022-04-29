253e1351b7c83315ace65dcd60bee56edc769855
PriyankaSahu88 <sahu.priyanka898@gmail.com>
2022-04-29 23:30:29 +0530

    Ecommerce_Site Project Created
    -project created
    -home app created
    -added static files
    -updated settings for static files

---
+++ ecommerce/home/urls.py
@@ -0,0 +1,10 @@
+
+import imp
+from django.contrib import admin
+from django.urls import path
+from home.views import home, about
+
+urlpatterns = [
+   path("", home, name="ssi-home"),
+   path("about", about, name="ssi-about")
+]
